c543685154a7ba881d3768635c23cb5c880a809e
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2014-10-31 07:19:12 +0000

    Rename field
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- readme/model.py
+++ readme/model.py
@@ -9,7 +9,7 @@ class Recommendation(Base):
     __tablename__ = conf.DB_TABLE
 
     id = Column('id', Integer, primary_key=True)  # implicit auto-increment
-    book_name = Column('book_name', String(length=100))
-    author = Column('author', String(length=100))
+    book_title = Column('book_title', String(length=200))
+    author = Column('author', String(length=200))
     category = Column('category', String(length=100))
     description = Column('description', Text())
